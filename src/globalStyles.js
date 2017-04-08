a02a430ea36ab2908f5a22635a43d4e84b8fe360
bAndelic <bruna.andelic@fer.hr>
2017-04-08 22:58:19 +0200

    other background image

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -4,13 +4,13 @@ import { injectGlobal } from 'styled-components'
 
 injectGlobal`
 
-  @import: url('https://fonts.googleapis.com/css?family=PT+Sans:400,900&amp;subset=latin-ext');
+  @import: url('https://fonts.googleapis.com/css?family=PT+Sans:400&amp;subset=latin-ext');
 
   body, html{
     height: 100%;
     margin: 0;
     background: white;
-    font-family: 'PT Sans';
+    font-family: 'PT Sans', sans-serif;
   }
   
   a {
