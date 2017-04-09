62ddbacdc3090bed3cc42ba23471fa4bb87f3c88
MSekrst <matija.sekrst13@gmail.com>
2017-04-09 05:42:40 +0200

    /stats/specific/:tag

--- custom/src/routes/statistics.js
+++ custom/src/routes/statistics.js
@@ -13,14 +13,46 @@ router.get('/all', (req, res) => {
     } else {
       const stats = {}
 
-      console.log('start', data.length);
-
       data.forEach(item => {
         if (stats[item.artikl.naziv]) {
           stats[item.artikl.naziv].counter += 1
         } else {
           stats[item.artikl.naziv] = {
             item, counter: 1,
+          }
+        }
+      })
+
+      const arr = [];
+
+      Object.keys(stats).forEach(i => {
+        if (stats.hasOwnProperty(i)) {
+          arr.push(stats[i])
+        }
+      })
+
+      res.status(200).json(arr);
+    }
+  })
+})
+
+router.get('/specific/:tag', (req, res) => {
+  const db = getDbConnection();
+
+  db.collection('Popis_Narudzbi').find().toArray((err, data) => {
+    if (err) {
+      res.status(500).end();
+    } else {
+      const stats = {}
+
+      data.forEach(item => {
+        if (item.tag === req.params.tag) {
+          if (stats[item.artikl.naziv]) {
+            stats[item.artikl.naziv].counter += 1
+          } else {
+            stats[item.artikl.naziv] = {
+              item, counter : 1,
+            }
           }
         }
       })
